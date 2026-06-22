7020c1b3cbb7c473badf167b7645fa4f4469e440
reyescarlosjesusalfredo <reyescarlosjesusalfredo@gmail.com>
2026-06-22 09:34:22 -0500

    Add files via upload

---
+++ Multiplo17.java
@@ -0,0 +1,13 @@
+public class Multiplo17 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 100; i++) {
+
+            if(i % 17 == 0) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
